b294c3b03ecf69268f879d67469d0776f3e85ed9
Anna Strom-Olsen <aso1124@gmail.com>
2021-06-22 11:40:47 -0400

    feat: add AutoSizer
    Wrap components in AutoSizer

--- nerdlets/session-timeline-nerdlet/index.js
+++ nerdlets/session-timeline-nerdlet/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {
+  AutoSizer,
   PlatformStateContext,
   NerdletStateContext,
   EntityByGuidQuery,
@@ -14,53 +15,58 @@ export default class Wrapper extends React.Component {
   render() {
     return (
       <PlatformStateContext.Consumer>
-        {platformUrlState => (
+        {({ timeRange }) => (
           <NerdletStateContext.Consumer>
-            {nerdletUrlState => {
-              // make sure the nerdlet has access to this entity
-              return (
-                <EntityByGuidQuery entityGuid={nerdletUrlState.entityGuid}>
-                  {({ data, loading, error }) => {
-                    if (loading) {
-                      return <Spinner fillContainer />
-                    }
-                    if (error) {
-                      return <BlockText>{error.message}</BlockText>
-                    }
+            {nerdletUrlState => (
+              <AutoSizer>
+                {({ width, height }) => (
+                  <div style={{ width, height, overflowX: 'hidden' }}>
+                    <EntityByGuidQuery entityGuid={nerdletUrlState.entityGuid}>
+                      {({ data, loading, error }) => {
+                        if (loading) {
+                          return <Spinner fillContainer />
+                        }
+                        if (error) {
+                          return <BlockText>{error.message}</BlockText>
+                        }
 
-                    if (
-                      data.entities &&
-                      data.entities[0] &&
-                      data.entities[0].guid
-                    ) {
-                      return (
-                        <ConfigProvider>
-                          <SessionTimelineContainer
-                            launcherUrlState={platformUrlState}
-                            nerdletUrlState={nerdletUrlState}
-                            entity={data.entities[0]}
-                          />
-                        </ConfigProvider>
-                      )
-                    } else {
-                      return (
-                        <div className="message">
-                          <HeadingText>
-                            Session Timeline is not available
-                          </HeadingText>
-                          <BlockText>
-                            You have access to this entity, but Session Timeline
-                            has not been enabled for Browser entities in this
-                            account. Please see your Nerdpack Manager to request
-                            access.
-                          </BlockText>
-                        </div>
-                      )
-                    }
-                  }}
-                </EntityByGuidQuery>
-              )
-            }}
+                        if (
+                          data.entities &&
+                          data.entities[0] &&
+                          data.entities[0].guid
+                        ) {
+                          return (
+                            <ConfigProvider
+                              entityGuid={nerdletUrlState.entityGuid}
+                            >
+                              <SessionTimelineContainer
+                                timeRange={timeRange}
+                                nerdletUrlState={nerdletUrlState}
+                                entity={data.entities[0]}
+                              />
+                            </ConfigProvider>
+                          )
+                        } else {
+                          return (
+                            <div className="message">
+                              <HeadingText>
+                                Session Timeline is not available
+                              </HeadingText>
+                              <BlockText>
+                                You have access to this entity, but Session
+                                Timeline has not been enabled for Browser
+                                entities in this account. Please see your
+                                Nerdpack Manager to request access.
+                              </BlockText>
+                            </div>
+                          )
+                        }
+                      }}
+                    </EntityByGuidQuery>
+                  </div>
+                )}
+              </AutoSizer>
+            )}
           </NerdletStateContext.Consumer>
         )}
       </PlatformStateContext.Consumer>
