04c0418529f6fafc9cd7dad47de26221220495f4
Anna Strom-Olsen <aso1124@gmail.com>
2022-01-04 13:38:31 -0500

    fix: eslint config and dependencies
    Update eslint to use newrelic eslint plugins
    Update eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:@newrelic/eslint-plugin-newrelic/react',
+    'plugin:@newrelic/eslint-plugin-newrelic/jest',
+    'plugin:@newrelic/eslint-plugin-newrelic/prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "prettier",
-        "prettier/react",
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        semi: false,
+      },
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "warn"
-    }
-};
+    'react/prop-types': 'warn',
+    'react/no-unused-state': 'warn',
+  },
+}
